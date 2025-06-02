149a7bf1a4f8fd13307487b9416c8cc6639aee07
Harsh Sharma <itisharshsharma18@gmail.com>
2025-06-02 09:18:31 +0530

    Changed the pre-input data to write to  user-inputed data and added some exeception handling to code.Updated some comments.

--- SyntaxAndMethods/FileHandling/Writing_Files.py
+++ SyntaxAndMethods/FileHandling/Writing_Files.py
@@ -1,76 +1,81 @@
-#writing files in python (.txt, .json, .csv)
+#writing files in python .txt, .json, .csv
+#Pending .xlsx and .zip
 
-txt_data = "I like pizza!"
+txt_data = input("Enter: ")
+txt_file_path = "/home/vhvhs/outputOfWrittingFile.txt"
 
-file_path = "/home/vhvhs/outputOfWrittingFile.txt"
-
-with open(file_path, "w") as file:# with is statement used to wrap the code i.e. closes the file as soon as it's done reading
+with open(txt_file_path, 'w') as file:# with is statement used to wrap the code i.e. closes the file as soon as it's done reading
     # open(), the first parameter will return as the file object 
-    # the second parameter, "w" is the writing mode
+    # the second parameter, 'w' is the writing mode for existing files, it also creates a new file if file does not exist.
         file.write(txt_data)
-        print(f"text file {file_path} is created now")
-
 try:
-    with open(file_path, "x") as file:# with is statement used to wrap the code i.e. closes the file as soon as it's done reading
+    with open(txt_file_path, 'x') as file:
     # open(), the first parameter will return as the file object 
-    # the second parameter, "w" is the writing mode
-    #it will throw an error if the file already exists
+    # the 'x' is EXCLUSIVE to only creating new files and writting them, it WILL raise an error if this file already exists.
         file.write(txt_data)
-        print(f"text file {file_path} is created now")
+        print(f"text file {txt_file_path} is created now")
 
-except FileExistsError:
+except FileExistsError:#needed for 'x' OpenTextMode
     print("This file exists already.")
-
-finally:
+except PermissionError:
+    print("Error: File read permission denied.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+else:
     print("For appending/updating a file's data. ")
-    txt_data2 = "I also like Healthy Food!"
+    txt_data2=input("Something to append: ")
 
-    with open(file_path, "a") as file:
+    with open(txt_file_path, 'a') as file:# appened mode
         file.write("\n"+txt_data2+"\n")
-        print(f"text file {file_path} is appended/updated now")
+        print(f"text file {txt_file_path} is appended/updated now")
 
     #for lists 
     print("For appending/updating a file's data.")
     employee_list = ["Eugene", "Mayweather", "Sourbee"]
 
-    with open(file_path, "a") as file:
+    with open(txt_file_path, 'a') as file:
         for employee in employee_list:
             file.write(employee+"\n")
-        print(f"text file {file_path} is appended/updated now for the list")
+        print(f"text file {txt_file_path} is appended/updated now for the list")
+
 
 import json
-
-print("For the json file.")#Json needs couples of key and values i.e. a dictionary
-
-employee_details = {"name":"SpongeBob SquarePants",
-            "age":12,
-            "Job/Profession":"Cook"}
-
-file_path2 = "/home/vhvhs/OutputOfJsonFile.json"
-
-with open(file_path2, "w") as file:
-        for key, value in employee_details.items():
-            file.write(f"{key}={value}\n")
-        print(f"Json file {file_path2} is created now")
-
+json_file_path="/home/vhvhs/OutputOfJsonFile.json"
+json_data={}
+iteration=int(input("How many data entries = "))
+for i in range(iteration):#setting up a user-input dictionary named json_data.
+    key = input(f"key {i} = ")
+    value= input(f"value of {key} = ")
+    json_data[key]=value
+print("-"*20)
+try:
+    with open(json_file_path, 'w') as json_file:
+        for k, v in json_data.items():
+            json_file.write(f"{k}={v}\n")
+        
+except FileNotFoundError:
+    print(f"Error: File not found at '{json_file_path}'")
+except PermissionError:
+    print("Error: File read permission denied.")
+except Exception as e:
+    print(f"An unexpected error occurred.")
 import csv
 
 print("For .csv files")
-#csv = comma separated values, they for data spreadsheets like Microsoft excel or Google Sheets
+#csv = comma separated values, they are used for data like Microsoft excel or Google Sheets
 
-employee_2D_collection = [["Name", "Age", "Profession"],
+employee_data = [["Name", "Age", "Profession"],
                           ["Harsh", "18", "Unemployed"],
                           ["Mango", "0", "Fruit"]] 
 
-file_path2 = "/home/vhvhs/OutputOfCSVFile.csv"
+csv_file_path = "/home/vhvhs/OutputOfCSVFile.csv"
 
-with open(file_path2, "w") as file:
+with open(csv_file_path, 'w') as file:
         
         writer = csv.writer(file)
-
-        for employee_row in employee_2D_collection:
-            writer.writerow(employee_row)
-        print(f"CSV file {file_path2} is created now")
+        for employee_data_row in employee_data:
+            writer.writerow(employee_data_row)
+        print(f"CSV file {csv_file_path} is created now")
 
     
 
