d5417fa9158cc0654e29c9aec1c71cb1f8800350
Harsh Sharma <itisharshsharma18@gmail.com>
2025-07-06 17:00:46 +0530

    Added a comment on conditional expression for better reading.

--- SyntaxAndMethods/BasicControlStructure/Logical_Operators_Terenary_operators.py
+++ SyntaxAndMethods/BasicControlStructure/Logical_Operators_Terenary_operators.py
@@ -22,9 +22,11 @@ elif (30>temp>10 and is_friends_available and not is_weather_good):
     #for int/float
 a=int(input("Enter a Number:"))
 #(f"")is not necessary here only "" is required
-even_or_odd = (f"{a} is Even") if a%2==0 else (f"{a} is Odd")
+even_or_odd = (f"{a} is Even") if a%2==0 else (f"{a} is Odd")#even_or_odd will store this if or else as a string 
 print(even_or_odd)
+print("Type of even_or_odd is ",type(even_or_odd))
     #string 
+
 user = "admin"
 access_level_identifier = "FULL ACCESS" if user=="admin" else "LIMITED ACCESS"
 print(access_level_identifier)
