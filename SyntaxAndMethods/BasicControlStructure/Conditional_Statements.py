1231dbe5e2542d5b20689e24ac68fe191875488d
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-09 22:38:47 +0530

    Updated the comments so they can describe the Conditional statements from best of my knowledge.

--- SyntaxAndMethods/BasicControlStructure/Conditional_Statements.py
+++ SyntaxAndMethods/BasicControlStructure/Conditional_Statements.py
@@ -1,6 +1,35 @@
- # if elif(else if) and else statments
-age = int(input("Enter your age: "))
+"""
+if, elif and else:
+"""
 
+"""
+A if statement is a user-defined condition to trigger a case or manipulate the control flow of the program
+If a specific condition is met then than the the If statement is triggered/True and the code snippet executes...
+
+If not(False) the code snippet's execution is skipped.
+
+elif(short for 'else if') statement is only exceuted when the if condition is false.
+There can be multiple elif conditions.
+Each elif statement can have its own condition(s).
+
+else condition is exceuted only when the if and elif conditions above it are False.
+An else condition can be thought as a safety net if all conditions fail above it.
+
+SYNTAX:
+if a_condition:
+   #Only when the condition is true.
+   /*Code here*/
+
+elif another_condition:
+   #Only executed when this condtion is True and if was False
+.
+.
+.
+else:
+   #only when if and all elif conditions were False
+"""
+
+age = int(input("Enter your age: "))
 if (age >=100):
    print("You are too old to sign up")
 elif (age >=50):
