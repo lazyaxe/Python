9ef771f0fd2f5fa405d0e99ea9cc89e1db5eb052
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-09 21:50:19 +0530

    Improved code readability.

--- SyntaxAndMethods/BasicControlStructure/User_Input_In_Python.py
+++ SyntaxAndMethods/BasicControlStructure/User_Input_In_Python.py
@@ -1,16 +1,20 @@
-# For Input data we use input() registers entered data as a string known as 'str' data type to make use of data we need to type cast it into a desired data type.
+"""
+For Input data we use input() registers entered data as a string known as 'str' data type .
+"""
 
 name=input("What is your name: ")
 age=input("Age = ")
-
-#this also works if you, don't want to store/access the user input anymore
-input("What is your name ? = ")
-
 # age will be returned as a string, we can't do arithmetic operations on a string
+print("Type of age variable:", type(age))
+
 #so we need to type cast age
 age = int(age)
 birth_year = 2025 - age
 print(f"{name}, your birth year is {birth_year}")
-# we can also type cast by :   
-sample1 = int(input("Is this more readable, 1 or 0 ?"))#input string will be typecasted into a int semi-automatically
+
+# we can also type cast directly by:   
+sample1 = int(input("Input a int:"))#input string will be typecasted into a int semi-automatically
 print(f"{name}, sample1 will now store data of",type(sample1))
+
+input("What is your name ? = ")
+#This does not store any value in the variable, but it works
