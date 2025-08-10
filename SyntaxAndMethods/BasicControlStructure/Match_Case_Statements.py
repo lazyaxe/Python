fef8bdc7e82f310f6c18e50f5911ecbdeb6b3ab5
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-10 13:18:18 +0530

    Added more understandable comments for better understanding of Match-cases.

--- SyntaxAndMethods/BasicControlStructure/Match_Case_Statements.py
+++ SyntaxAndMethods/BasicControlStructure/Match_Case_Statements.py
@@ -1,4 +1,11 @@
-#Match Case conditions are similar to switch case of C/C++
+"""
+Match-Cases:
+
+    Match-cases are very similar in terms of functionality in comparasion to if-elif-else statements as it also manipules the control flow.
+
+    Match Case conditions are equivalent to switch case in C/C++.
+"""
+
 def day_of_the_week(day):
     match day:
         case 1:
@@ -15,8 +22,28 @@ def day_of_the_week(day):
             return "Friday"
         case 0: #because 7%7=0
             return "Saturday"
-        case _: #default:
+        case _: #default case, similar to else
             return "Invaild input"
+"""
+equivalent if else ladder:
+
+if day=="Sunday":    
+    return 1
+elif day=="Monday":
+    return 2
+elif day=="Tuesday":
+    return 3
+elif day=="Wednesday":
+    return 4
+elif day=="Thrusday":
+    return 5
+elif day=="Friday":
+    return 6
+elif day=="Saturday":
+    return 7
+else: 
+    return "Invaild input"
+"""
 is_loop = True
 while is_loop:
     day = input("Enter a day: ")
