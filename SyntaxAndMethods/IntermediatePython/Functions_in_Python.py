070b8dc1f4bedb729c569c647d18918a7bd92dc7
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-10 20:48:49 +0530

    Added comments explaining the definition of Function and types of function etc...

--- SyntaxAndMethods/IntermediatePython/Functions_in_Python.py
+++ SyntaxAndMethods/IntermediatePython/Functions_in_Python.py
@@ -1,8 +1,47 @@
-#Functions in python
-def invoice_display(username, amount, DueDate):#def function_name("parameter1",parameter2)
+"""
+Functions in python:
+    Functions are a re-usable block of code.
+    A function can be called anywhere and at anytime as many times as needed.
+
+    def <function_name>(parameter_1, parameter_2, ...):
+        #re-usuable code here
+        return variable
+
+    #function call
+    <function_name>(arguement1, arguement2, ...)
+    
+    A function has two requirements: Arguements and return
+    SYNTAX:
+    
+    So there can be four vaild function combinations:
+    1. No Arguments(args), No return
+
+        def <function_name>():
+        #re-usuable code here
+
+    2. Agruements, No return
+    
+        def <function_name>(parameter_1, parameter_2, ...):
+        #re-usuable code here
+        
+
+    3. No arguments, return
+    
+        def <function_name>():
+        #re-usuable code here
+        return variable
+
+    4. Arguement, return 
+
+        def <function_name>(parameter_1, parameter_2, ...):
+        #re-usuable code here
+        return variable
+"""
+
+def invoice_display(username, amount, DueDate):#def <function_name>(parameter1, parameter2)
     print(f"Hello {username} !")
     print(f"Your bill is {amount:,.2f} rupees")
-    print(f"Bill  must be paid before {DueDate}")
+    print(f"Bill must be paid before: {DueDate}")
     print()
 invoice_display("Harsh", 20154.56,"21/03/25")#feeding the arguements to function
 
