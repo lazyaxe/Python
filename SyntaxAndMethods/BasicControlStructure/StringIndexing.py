3c7eb5f23538eca90d95f2bdbd3e4bef190dcdc7
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-10 13:59:29 +0530

    Refactored the comments to clear.

--- SyntaxAndMethods/BasicControlStructure/StringIndexing.py
+++ SyntaxAndMethods/BasicControlStructure/StringIndexing.py
@@ -1,34 +1,46 @@
-#index = acessing elements of a sequence using [], []=indexing operator
-#   [start of string:end of string:step of string]
+"""
+indexing is the way to access the elements of a string, list, array, tuple etc...
+[]=indexing operator
+"""
 
+"""
+Slicing:
+Slicing is a method in Python by which we can extract required data from a string, list, tuple etc...
+
+<name>[start of string:end of string:step of string]
+"""
 credit_card_number="1212-3264-2184"
-print(credit_card_number[0])#prints the first char of string(0-13)
-        #1
-        
-print(credit_card_number[0:4])#prints till the 0th to 4th elements
-        #1212
+print(len(credit_card_number))#14 chars
 
-print(credit_card_number[:4])#this will print from the start(0th) to 4th element
-        #1212
+print(credit_card_number[0])
+#prints the first char of string i.e. '1'
 
-print(credit_card_number[5:])#this will print from the 5th element to the last element of string
-        #3264-2184
+print(credit_card_number[0:4])
+#prints till the 0th to 4th element i.e. 1212
 
-#we can also use the negative indexing
-print(credit_card_number[-1])#this prints the last element
-        #4
+print(credit_card_number[:4])
+#equivalent to credit_card_number[0:4]
+#print from the starting char(i.e. credit_card_number[0]) to credit_card_number[4] i.e. 1212
 
-#we can also step (skip number by a pattern)
-print(credit_card_number[::2])#this prints from the 0th element to last element but skips every element of index that is multiple of 2  
-        #11-2428
+print(credit_card_number[5:])
+#this will print from the 5th element to the last element of string i.e. 3264-2184
 
-#to print the last digits of credit card
-last_digits_of_card=credit_card_number[-4:]#this will print from the last fourth element to last element
-        #2184
+#Negative indexing
+print(credit_card_number[-1])
+#this prints the last element i.e. 4
+
+#Step (skip number by a definite pattern)
+print(credit_card_number[::2])
+#this prints from the 0th element to last element but skips every element of index that is multiple of 2  
+#11-2428
+
+#To print the last digits of credit card
+last_digits_of_card=credit_card_number[-4:]
+#this will print from the last fourth element to the last element
+#2184
 print(f"XXXX-XXXX-{last_digits_of_card}")
 
-#reverse the string
+#To reverse the string:
 credit_card_number=credit_card_number[::-1]
+#This will traverse throught the array but in the reverse order
 print(credit_card_number)
-print("or")
-print(credit_card_number[::-1])
