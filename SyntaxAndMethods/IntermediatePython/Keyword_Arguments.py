9fcb3d0660488357177b1cc5bb6c57b89714298a
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-12 23:35:19 +0530

    Improved previous comments/documentation.

--- SyntaxAndMethods/IntermediatePython/Keyword_Arguments.py
+++ SyntaxAndMethods/IntermediatePython/Keyword_Arguments.py
@@ -1,11 +1,17 @@
-#Keyword Arguments == and argument preceded by an identifier
-    #helps with readability 
-    #order of arguments don't matter in Keyword Args("NON POSITIONAL")
-    #A POSTIONAL ARGUMENT COMES FIRST THEN KEYWORD ARGUMENTS
+"""
+Keyword Arguments:
+    Keyword Arguments are the arguements preceded by an identifier.
+    Keyword Arguments helps with readability.
+    Order of arguments don't matter in Keyword Args("NON POSITIONAL")
+    A positional arguement comes first before keyword arguments.
+    SYNTAX:
+        def <function_name>(positonal_parameter1, keyword_parameter1, keyword_parameter2):
+            #code
 
+        <function_name>(positonal_parameter1, keyword_parameter2="string", keyword_parameter2=1234)
+"""
 def generate_greet(greeting, title, FirstName, LastName):
     print(f"{greeting}! {title} {LastName} {FirstName}")
 
-#now USING KEYWORD ARGUEMENTS
-#syntax:-  function_name(positonal_parameter, parameter1="string", parameter2=1234)
+#now using keyword arguements:
 generate_greet("Namaskaram",FirstName="Ainz",title="Sorccer King",LastName="Gown Ool" )
