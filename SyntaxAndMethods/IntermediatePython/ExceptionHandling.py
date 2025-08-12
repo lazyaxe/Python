909676de74b63ea4c7cf36ebc63378731c84af25
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-12 23:36:19 +0530

    Added comments to explaining the definition of exception handling and its syntax.

--- SyntaxAndMethods/IntermediatePython/ExceptionHandling.py
+++ SyntaxAndMethods/IntermediatePython/ExceptionHandling.py
@@ -1,22 +1,34 @@
-# Excecption is an event/error that interrupts the flow of the program like ZeroDivisionError, TypeError (like operation on a wrong datatype), ValueError (invaild type conversion str->int )
+"""
+Execption:
+    Excecption is an event/error that interrupts the flow of the program like ZeroDivisionError, TypeError (like operation on a wrong datatype), ValueError (invaild type conversion str->int )
 
-#1. try (executing a code snippet)
-#2. except (catching the error)
-#3. finally (crucial for clean ups like closing a open file after work is done)
+There are 4 parts in exception handling:
 
+1. try (executing a code snippet)
+2. except (catching the error)
+3. else (if try block succeeds)
+4. finally (crucial for clean ups like closing a open file after work is done)
+"""
+import logging as log
 try:
-    number = int(input("Enter a number only (not zero) = "))
-    print(1/number)
+    number= int(input("Enter a number only (not zero) = "))
+    if number==0:
+        raise ZeroDivisionError
     
-except ZeroDivisionError:#if zero is inputted
-    print("Told you to not input zero.")
+except ZeroDivisionError as ZDE:#if zero is inputted
+    ZDE="Divison by zero"
+    print(ZDE)
 
 except ValueError:#if a string or char is inputted
     print("Only Integers except zero please.")
 
-except Exception:
+except Exception as e:
+    log.exception(e)
     print("This catches any exception like an else statement, so put this at last")
+
+else:
+    print(1/number)
 
 #finally executes even after a exeception
 finally:
-    print("This need some clean up.")
+    print("This needs some clean up.")
