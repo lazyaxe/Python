f80d558cf76ad580ab76c9233203be219350cd02
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-12 23:34:58 +0530

    Improved previous comments/documentation.

--- SyntaxAndMethods/IntermediatePython/default_arguments.py
+++ SyntaxAndMethods/IntermediatePython/default_arguments.py
@@ -1,5 +1,11 @@
-#default arguements= they add a default value to choosen parameter to make function calling more flexible and reduce the mannual feeding of arguements 
-def net_price(price, discount=0, sales_tax=0.05):#because most people get 0% discount and 0.05% sales tax, feeding those parameter is no longer required, i.e they are optional now
+"""
+Default arguements:
+They add a default value to choosen parameter to make function calling more flexible and reduce the mannual feeding of arguements.
+""" 
+def net_price(price, discount=0, sales_tax=0.05):
+    """
+    because most people get 0% discount and 0.05% sales tax, feeding those parameter is no longer required, i.e they are optional now
+    """
     return price + (1-discount)*(sales_tax)
 
 customer1=net_price(500)#discount and sales tax are defaulted to pre-set value i.e. 0, 0.05 
