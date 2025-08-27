c52a8870e9293d074dd7ac2a94d8043ca10343f5
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-27 13:35:19 +0530

    A rework to my file that created a while ago...

--- SyntaxAndMethods/PythonOOP/classes_and_objects.py
+++ SyntaxAndMethods/PythonOOP/classes_and_objects.py
@@ -1,14 +1,38 @@
-class car:
-    #to create a constructor(dunder method) in py
-    def __init__(self, model, colour, is_car_sale):
-        #to include the object itself in the constructor as name 'self'.
-        #we need self to access other attributes(parameter for the constructor function) or its members
-        #self.attribute_name = recived_attribute
+"""
+   A Class is a blueprint to creating a object
+   A class has class members/attributes, class functions/methods, constructor and destructor...which the object/instance will have.
+   An attribute/A class members are basically the variables inside the class...
+
+   example: A class/blueprint of a Student might be:
+            Attributes/class
+                Their age, grade, roll number, etc...
+            Their methods will be study(), go_to_school() etc..
+   An Object/Instance is a bundle of methods and functions and class members which are implimented by classes 
+"""
+class Car:
+    def __init__(self, model, colour="White", is_car_sale=True):
+        """
+            What is a constructor in python?
+            ~> A constructor in Python is a dunder(double underscore) method by which the initialization of the attributes/class members is done directly when creating am instance of the class via arguements...
+            
+            ~>Since constructor is just a method i.e. A function inside a class default, keyword, *args, **kwargs can be used... 
+            
+            ~>The assignment of the values to attributes is done inside the constuctor method. 
+              By self.attribute=value
+            
+            ~> The self parameter is the identifier for the object 
+            
+            ~> So, instead of making instance_name.attribute=value for n different instances, the self keyword is only used the class .
+        """
         self.model = model
         self.colour =  colour
         self.is_car_sale = is_car_sale
-    #creating functions inside a class
+        
+    #creating methods (functions inside a class)
     def drive(self):
+        """
+            The self.attribute_name is used for accessing the value of an attribute...
+        """
         print(f"You start driving {self.model}.")
     def stop(self):
         print(f"You stopped driving {self.model}")
@@ -16,3 +40,21 @@ class car:
         print(f"Model {self.model}")
         print(f"{self.model}'s Colour = {self.colour}")
         print(f"Sale Status = {self.is_car_sale}")
+
+if __name__=='__main__':
+    """
+    Creation of an instance/object
+    """
+    car1=Car(model="Ford Mustang")
+
+    car2= Car(model="Ford F-150", colour="Black", is_car_sale=True)
+    """
+    Accessing the values of an object/instance
+    SYNTAX: 
+        instance_name.attribute
+    """
+    car1.colour="Pink"
+    print(f"The colour of {car1.model} is {car1.colour}")
+
+
+    print(f"The colour of {car2.model} is {car2.colour}")
