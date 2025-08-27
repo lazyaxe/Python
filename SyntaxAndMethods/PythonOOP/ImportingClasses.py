e061cf2af3b765b24248ebe6bec96a1337191458
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-27 22:22:07 +0530

    Updated comments and changed variable name "is_car_sale" to "is_for_sale"

--- SyntaxAndMethods/PythonOOP/ImportingClasses.py
+++ SyntaxAndMethods/PythonOOP/ImportingClasses.py
@@ -1,23 +1,24 @@
-#An Object is a bundle of methods and functions and variables example: phonebook and book or Student
-#A Class is a blueprint of creating a object
+"""
+    Importing the classes and objects from local modules...
+"""
 
-from classes_and_objects import car
-# or import CLASSexample as CLASS
+#Importing a class
+from classes_and_objects import Car
 
 #creating a object named "car_1"
-car_1=car(model="Skibidi", colour="brown", is_car_sale=True)#Initalizing the constructor value using keyword arugments
+car_1=Car(model="Tata Harrier", colour="Black", is_car_sale=True)#Initalizing the constructor value using keyword arugments
 
 #printing the attributes
 print(car_1.model)
 print(car_1.colour)
-print(car_1.is_car_sale)
+print(car_1.is_for_sale)
 
-car_2= car("Alto 500", "Red", False)#Initalizing the constructor value
+car_2= Car("Alto 500", "Red", False)#Initalizing the constructor value
 
 #printing the attributes
 print(car_2.model)
 print(car_2.colour)
-print(car_2.is_car_sale)
+print(car_2.is_for_sale)
 
 #object 1 accesing the class's methods 
 car_1.drive()
