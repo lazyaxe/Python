822d706563e41cde9e91658d669756fd7ecceaeb
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-30 19:32:46 +0530

    Changes: 1. Added more code documentation and purpose of code 2. Added the explaination in case of inherritance

--- SyntaxAndMethods/PythonOOP/@classmethod.py
+++ SyntaxAndMethods/PythonOOP/@classmethod.py
@@ -1,42 +1,69 @@
-#Allow operations related to class itself, mostly operations on class variables                                                               
-# takes 'cls' as the first parameter, represents the class itself .
+"""
+CLASS METHOD DECORATOR:
+~> @classmethod is a decorator declared before decalring a method for class i.e. a classmethod().
 
-class student:
+What is a class method?:
+~> Class method here refers to method which is bound/for a class rather than it's instance/object.
+~> Because it's bound to class, it takes 'cls' short for class as the first parameter rather than the 'self'.
+~> The @classmethod decorator calls the classmethod() internally.
+~> A class method can access or modify the class's attributes/variables while a static method can't access or modify it.
+~>class variables/attributes can be accessed directly but its recommended to use a classmethod as a factory method
+
+What is a Factory method:
+~>A factory method returns the class objects(like a constructor/__init__ method).
+"""
+
+
+class Student:
+#Class variables|attributes:
     student_count = 0
-    total_gpa=0
 
-    def __init__(self, name, gpa):
+    def __init__(self, name: str="Unknown", gpa: int|float=0)->None:
+    #instance attributes:
         self.name = name
         self.gpa = gpa 
-        student.student_count += 1
-        student.total_gpa+=gpa #student.total_gpa = student.total_gpa + gpa
-    #now we need a instance method to show info 
-    def get_info(self):
-        return f"{self.name} = {self.gpa}"
+        Student.student_count += 1
     
-    #now we need class method to access the class variable
+#classmethod to access the class variable 'student_count'.
     @classmethod
-    def get_count_students(cls):
-        return f"Total number of students = {cls.student_count}"
+    def get_student_count(cls)-> int:
+        return cls.student_count
+
+class Undergraduate(Student):
+#The class attributes and classmethod are inherrited by sub-class from base-class
+# i.e Undergraduate inherits student_count, get_student_count() by Student
     
+    degree="B.Tech in Computer Engineering Specialization in Artifical Intelligence and Machine Learning"
+    
+    ug_count=0
+
+    def __init__(self, name: str="Unknown", gpa: int|float=0)->None:
+        super().__init__(name, gpa)
+        Undergraduate.ug_count+=1
+
     @classmethod
-    def get_average_gpa(cls):
-        if cls.student_count <= 0:
-            return 0;
-        else:
-            return f"{cls.total_gpa/cls.student_count}"
-    
-student1=student(name="H1N1", gpa=4.5)
-print(student.student_count)
-print(student.total_gpa)
+    def get_degree(cls)-> str:
+        return cls.degree
 
-student2=student(name="V8", gpa=8)
-print(student.student_count)
-print(student.total_gpa)
+student1=Student(name="Prem", gpa=9.4)#the class variable will be incremented by 1. Now student_count=1
+print(Student.student_count)
 
-student3=student(name="Snapdragon", gpa=7)
-print(student.student_count)
-print(student.total_gpa)
+student2=Student(name="Vivek", gpa=9.5)#Now student_count=2
+print(Student.student_count)
 
-print(student.get_count_students())
-print(student.get_average_gpa())
+student3=Student(name="Sheprie", gpa=7)#Now student_count=3
+print(Student.student_count)
+
+print(Undergraduate.ug_count)
+
+student4=Undergraduate(name="Harsh Sharma", gpa=9.1)
+print(Student.student_count)
+"""
+FYI:
+~> The student_count attribute will be incremented, even if an instance from Undergraduate class will be created.
+~> Why? Because the Undergraduate class used the constructor of Students class using the super().__init__(). The super function access the constructor method which triggers the increment of student_count.
+~> The Undergraduate.student_count() will be lesser or equal to Student.student_count().
+"""
+print(Undergraduate.ug_count)
+
+
