67b60f2c12b23dce9e2ac3ccb3c54fee840fd339
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-30 19:36:47 +0530

    Changes: 1. Improved my code documentation. 2.  Added a better example to display the purpose this method.

--- SyntaxAndMethods/PythonOOP/@staticmethods.py
+++ SyntaxAndMethods/PythonOOP/@staticmethods.py
@@ -1,20 +1,45 @@
-#static methods = a method that belongs to an class rather than an instance(object of that class) that do not need any class data
+"""
+Static Methods:
+~> A static method in a python is a method that is used when some method when a method does not need any class attributes or the instance's attribute.
+~> Simply put, A static method is a method which does not uses the attributes of an instance or class.
+~> Therefore, static method requires neither a 'cls' or 'self' parameter.
+~> It can be put inside a class with the help of @staticmethod.
+~>Or can be simply put outside the class.
+~> A staticmethod cannot access/modify class attributes.
+"""
 
-#For Operations in instances, Instance methods are used.Instance methods take 'self' as the parameter
+#Example:
+class Calculator:
 
-class employee:
-    def __init__(self, name, job):
-        self.name=name
-        self.job=job
-
-    def get_employee_info(self):
-        return f"{self.name} is a {self.job}" 
-    #if we need to check for valid job positions in an office(that do not require class data)
-    @staticmethod
-    def isValidJob(job):
-        vaild_jobs=("Manager", "Engineer", "HR", "Janitor")
-        return job in vaild_jobs# this would return a bool
+    def __init__(self, company_name: str, model: str) -> None:
+        self.company_name=company_name
+        self.model=model
     
-emp1=employee(name="Harsh", job="Mage")#Because Mage job is not in valid_job isVaildJob will return False
-print(emp1.get_employee_info())
-print(f"Is {emp1.job} job vaild ?",employee.isValidJob(emp1.job))#used class name to access the method not the object's name
+    def get_info(self)->str:
+        return f"{self.company_name} {self.model}"
+    
+    @staticmethod
+    def add(*numbers: int|float)->int|float:
+        total=0
+        for number in numbers:
+            total+=number
+        return total
+    
+    @staticmethod
+    def div(*numbers: int|float)->float:
+        if 0 in numbers:
+            return 0
+        else:
+            result=0
+            for number in numbers:
+                result= numbers[0]/number
+            return result
+
+c1: Calculator=Calculator(company_name=
+"Casio", model="MJ-120GST")
+
+print(c1.get_info())
+
+print(c1.add(1, 2.5, 3))
+
+print(c1.div(5, 0.5))
