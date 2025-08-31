b113580d156b4f0b0a37c77a8c42f6a55a111abc
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-31 20:33:11 +0530

    Changes: 1. Implemented property() and @property decorator. 2. Added better class to highlight the differences between property() and @property.

--- SyntaxAndMethods/PythonOOP/@property.py
+++ SyntaxAndMethods/PythonOOP/@property.py
@@ -3,53 +3,107 @@
 # by property we can add additional logic when to read write or delete code 
 
 # @property gives you getter setter or deleter method
+"""
+What is a property() method:
+~>The property method lets a method to be called as an attribute.
+~> It provides a better way to implement encapsulation i.e. A better way to implement Getter and Setters
 
-class rectangle:
+Parameters of property():
+    ~>fget() used to get the value of attribute.
+    ~>fset() used to set the value of attribute.
+    ~>fdel() used to delete the attribute value.
+    ~>doc() string that contains the documentation (docstring) for the attribute.
 
-    def __init__(self, width, height):
-        self._width=width#set as protected because @property can be used as getter and setters
-        self._height=height#set as protected because @property can be used as getter and setters
+What is a @property decorator:
+~> The property decorator is a decorator for property method of the class
+~> It provides a cleaner way to use property().
+"""
 
-    @property# this is called to define a getter method
-    def width(self):#now width is the property name of this decorator 
-        return f"{self._width:.2f}cm"
-
-    @width.setter # @<propertyName>.setter
-    def width(self, new_width):
-        if new_width>0:
-            self._width=new_width
+# 1. Using property()
+class Square:
+    def __init__(self, length: int|float) -> None:
+        self.__length=length # private instance attribute   
+    
+    def get_length(self)-> int|float:
+        return self.__length
+    
+    def set_length(self, new_length: int|float):
+        if new_length<0:
+            print("Lenght cannot be less than zero")
         else:
-            print("Width should be positive")
+            self.__length=new_length
 
-    @width.deleter
-    def width(self):
-        del self._width
-        print("Width Deleted.")
+    def delete_lenght(self)->None:
+        if self.__length<=0:
+            print("Nothing to delete")
+        else:
+            print(f"Deleted: {self.__length}")
+            del self.__length
+    side=property(fget=get_length, fset=set_length, fdel=delete_lenght)
+
+# 2. Using @property
+class Rhombus:
+    def __init__(self, length: int|float) -> None:
+        self.__length=length
+    """
+    Syntax of @property:
+    ~> All the methods in the @property need have same names.
+    ~> With decorators, you're chaining functionality to the property by assigning handlers to that same property name.
+    ~> If names differ like get_length and length—Python won't connect them, causing functionality breaks.
 
     @property
-    def height(self):
-        return f"{self._height:.2f}cm"
- 
+    def attribute(self)->attribute
+        return attribute
 
-    @height.setter
-    def height(self, new_height):
-        if new_height>0:
-            self._height=new_height
+    @attribute.setter
+    def attribute(self, new_value)->None
+        self.attribute=new_value
+
+    @attribute.deleter
+    def attribute(self)->None
+        del self.attribute
+    """
+    @property
+    def length(self)->int|float:
+        #length now is the property name, this is the getter method.
+        return self.__length
+    @length.setter
+    def length(self, new_length)->None:
+        if new_length>0:
+            self.__length=new_length
         else:
-            print("Height should be positive")
+            print("Width can't be negative")
 
-    @height.deleter
-    def height(self):
-        del self._height
-        print("Height deleted.")
+    @length.deleter
+    def length(self)->None:
+        del self.__length
+        print("Width Deleted.")
+    """
+    Why the @lenght.getter isn't needed?
+    ~>The @property decorator is basically a syntactic sugar for property(). 
+    ~> So the use of @property inits the method property:
+        length = property(length)
+    ~> Now lenght is property and where the setter is defined. And it is the first method that the @property expects
+    ~> Now, there's a @length.getter method but it's for later use when there's a need to re-define/override 
+    """
 
-rectangle1 = rectangle(width=2, height=5)
+s1= Square(length=10)
 
-rectangle1.width = float(input("Enter the new width = "))
-print(rectangle1.width)
+print("Lenght of square: ", s1.side)#This will call the fget=get_length
 
-rectangle1.height = float(input("Enter a new height = "))
-print(rectangle1.height)
+s1.side=56#This will call the fset=set_length
 
-del rectangle1.width#rectangle1._width i.e the _width attribute of the instance rectange1 has been deleted
-del rectangle1.height
+print("New lenght of square: ", s1.side)
+
+del s1.side# will delete the attribute lenght from the memory
+
+r1 = Rhombus(length=2)
+
+r1.length = float(input("Enter the new length = "))
+print(r1.length)
+
+r1.length = float(input("Enter a new length = "))
+print(r1.length)
+
+del r1.length#rectangle1._width i.e the _width attribute of the instance rectange1 has been deleted
+del r1.length
