916d679eadd051f1f353d198903f5b657b4d6165
Harsh Sharma <itisharshsharma18@gmail.com>
2025-09-01 20:27:42 +0530

    Re-implemented the hardest topic for me(in Python) yet: decorators Changes: 1. Updated explaination. 2. better examples 3. I finally got the intuition!!!

--- SyntaxAndMethods/PythonOOP/decorators.py
+++ SyntaxAndMethods/PythonOOP/decorators.py
@@ -1,39 +1,133 @@
-#decorator is a function that extends/adds a behavior of a function without modifying the base function.
+"""
+What is a decorator:
+~> In Python, decorators are flexible way to modify or extend behavior of functions or methods, without changing their actual code.
 
-#How? Pass the base function as an argument to the decorator.
+~> A decorator is essentially a function that takes another function as an argument and returns a new function with enhanced functionality.
 
-# How to make a decorator? 
+~> Decorators can be:
+    1. used alone as functions i.e. @function_decorator 
+    2. used for classes i.e. @class_decorator
+    3. or used for methods i.e. @method_decorator
 
-#def add_sprinkles(function_name):
-#    def wrapper():
-#        return function_name
+~> Decorators are often used in scenarios such as logging, authentication and memorization, allowing us to add additional functionality to existing functions or methods in a clean, reusable way.
 
-def add_sprinkles(function_name):# add_sprinkles is now a decorator
+~>The decorator function/method takes the function which functionality needs to be modified as a argument. 
 
-    def wrapper(*args, **kwargs):# *args and **kwargs are added to accept any amount/type of argument that the base function i.e. ice_cream might have
-        print("You've added sprinkles to your Ice cream!")
-        function_name(*args, **kwargs)
-    return wrapper
+~> Many times the wrapper method has *args and **kwargs to accept any number of arguments.
 
-def add_fudge(function_name):# add_fudge is now a decorator
-    
+SYNTAX:
+def decorator(function_name):
     def wrapper(*args, **kwargs):
-        print("You've added fudge!")
-        function_name(*args, **kwargs)
-    return wrapper
+        return function_name
 
-def add_whipped_cream(function_name):# add_fudge is now a decorator
+@decorator//function decorator
+def function()
+    pass
     
+@decorator//class decorator
+class C:
+    pass
+    
+    @decorator//method deorator (not so common)
+    def method()
+        pass
+"""
+
+"""
+EXAMPLE:
+Evaluation of time for execution of a function
+by start time - ending time.
+"""
+from time import time, sleep
+from unittest import result
+
+start_time=time()
+def print_number()->None:
+    sleep(2)#delayed execution
+    print(69)
+
+print_number()
+print(f"print_number {time()-start_time}")
+
+
+"""
+This is a not an efficient method since every function would it's own difference time thingy.
+
+We can use the two ways to generalize it:
+    1. Make a timer function 
+    2. Use decorators
+"""
+def timer(function):
     def wrapper(*args, **kwargs):
-        print("Whipped cream added!")
-        function_name(*args, **kwargs)
+        start_time = time()
+        result = function(*args, **kwargs)
+        print(f"{function.__name__} took {time() - start_time:.2f}s")
+        return result
     return wrapper
 
-@add_sprinkles
-@add_fudge
-def ice_cream(flavour="Vanilla"):
-    print(f"Here's your {flavour} Ice cream! 🍦")
+"""
+1. Timer function 
+"""
+print_number = timer(function=print_number)
+"""
+Explaination: 
+~>print_number is the function object not a function, kind of a reference to the function.
+~>the print_number object now references to timer(print_number) which has argument of print_number
+~> Now the timer(print_number) is executed and the the return value wrapper is passed to print_number object
+~>*args, **kwargs arguments of wrapper may seem useless because, there are no arguments in print_number. But *args, **kwargs are just for a safety case here, they will be ignored.
+~>Now print_number references to wrapper(*args, **kwargs).
+~> so in the end, 
+print_number()=wrapper():
+                start_time = time()
+                result = function()
+                print(f"{function.__name__} took {time() - start_time:.2f}s")
+                return result
 
-ice_cream = add_whipped_cream(ice_cream)
-ice_cream(flavour="Strawberry")#Calling the function in main
-#Alternative of @add_whipped_cream
+~>Now we have successfully "re-defined/modified" the print_number() function to also measure the time. 
+
+What about the wrapper function: 
+~> Because we didn't "call" the wrapper function, it we just returned the function's reference to print_number
+"""
+print_number()
+
+"""
+2. timer decorator
+
+This is just syntactic sugar the interpreter converts this @decorator to function=decorator(function)
+"""
+@timer
+def print_num()->None:
+    sleep(2)
+    print(420)
+
+print_num()
+
+"""
+Example 2: displaying the arguments passed in a function
+"""
+def args_info(function):
+    def wrapper(*args, **kwargs):
+        result=function(*args, **kwargs)#greet(*name, greeting)
+        print(f"{function.__name__}:")
+        print("Args used: ", args)#because args are basically a list of arguments...
+        print("kwargs used ", kwargs)#because kwargs are basically a dict of arguments
+        return result
+    return wrapper
+
+@timer
+@args_info
+def greet(*name, greeting):
+    sleep(2)
+    full_name=""
+    for word in name:
+        full_name=full_name + " " + word
+    return f"{greeting}{full_name}"
+
+"""
+The @args_info is executed first because it's like inverted stack.
+
+It's functionally equivalent to: 
+
+greet=timer(args_info(function=greet))
+"""
+print(greet("Harsh", "Sharma", greeting="Hello"))
