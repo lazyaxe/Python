d091b68b2afe9c3aa6d37e494c391a37cf69d8de
Harsh Sharma <itisharshsharma18@gmail.com>
2025-10-02 16:06:50 +0530

    Changes made: 1. Corrected some explainations 2. Added explanation for nested if-else chain. 3. Replaces 'i' of the for loop with a better name. 4. Added better argument names for map_list.

--- SyntaxAndMethods/IntermediatePython/lamda-functions.py
+++ SyntaxAndMethods/IntermediatePython/lamda-functions.py
@@ -1,16 +1,14 @@
 """
 Lambda functions:
-    Lambda function in pythons is basically a one line function.
-    
-    Lambda functions are also called anynoumous functions because they usually do not have a identifier/name.
-    
-    Main use of lambda function is in Higher order functions(When functions are given in other funcions as arg.)
+    ~>Lambda function in pythons is basically a one line function.
+    ~>The lambda function always returns the value
+    ~>Lambda functions are also called anynoumous functions because they usually do not have a identifier/name.
+    ~>So, they usally used as an one-time-call-function.
+    ~>If you want to recall a lambda function, you should initialize it with a name. Now the lamba function can be called as a regular function
+    ~>Main use of lambda function is in Higher order functions(When functions are given in other funcions as arg.)
 
-    Lambda 
     SYNTAX:
-        lamba <variable1>, <variable2>, ...:<variable1> <operation> <variable2>.... 
-
-        The operation result is always returned.
+        lamba <variable1>, <variable2>, ...: <variable1> <operation> <variable2>.... 
 """
 
 add=lambda x,y: x+y
@@ -18,56 +16,64 @@ print(add(4,5))
 
 result=lambda x,y: x+y(4,5)
 
-"""
-Lambda functions with condition checking:
-    Lambda function's condition always require an else statement.
-"""
-
-number_checker=lambda x: "Positive" if x>0 else "Negative" if x<0 else "Zero"
-print("Result=", number_checker(4))
-
+#Lambda functions with condition checking:
+"""   Lambda function's condition always require an else statement."""
 
 compare=lambda x, y: f"{x} is greater than {y}" if x>y else f"{y} is greater than {x}"
 print(f"Comparision: {compare(x= int(input("Enter the first number: ")), y= int(input("Enter the second number: ")))}")
 
+#Nested if else chain
+number_checker=lambda x: f"{x} is a positive integer" if x>0 else f"{x} is a negative integer" if x<0 else "Zero"
+print("Result=", number_checker(4))
+'''
+EQUIVALENT TO:
+    def number_checker(x):
+        if x>0:
+            return f"{x} is a positive integer"
+        else:
+            if x<0:
+                f"{x} is a negative integer" 
+            else "Zero"
 
+print("Result=", number_checker(4))
+'''
+
+#String operations 
 string = input("Enter a string: ")
-operation = lambda char: char.upper() if char.isalpha() else char
-result = ''.join(operation(char) for char in string)
-print("String operation:", result)
+upper_operation = lambda char: char.upper() if char.isalpha() else char
+upper_result= ''.join(upper_operation(char) for char in string)
+print("String operation:", upper_result)
 
+#Using lambda function in list Comprehension:
 """
-Lambda function with List Comprehension:
+    ~>The functions can be used by calling a list's element...
+    "arg=x" in lambda arg=x: arg*10 is a default arg that equals to whatever the value of x was during the for loop. 
+    Each sample_list item/element stores a different value of default arg
 """
 
 sample_list=[lambda arg=x: arg*10 for x in range(1, 5)]
-"""
-    This is the creation of a list of lambda functions.
-    The functions can be used by calling a list's element...
-    "arg=x" in lambda arg=x: arg*10 is a default arg that equals to whatever the value of x was during the for loop. 
-    Each sample_list item/element stores a diffenrent value of default arg
-"""
 
 print(f"Lambda function at 0th item of the list: {sample_list[0]()}")
 
 counter=0
-for i in sample_list:
-    print(f"Lambda function at item {counter} of the list: {i()}")
+for lamda_function in sample_list:
     """
-    i is a lamda function, not a literal, so it requires a parenthesis and because it a has a default arg equivalent to: sample_list[0]()
-    since no args are given, each sample_list item/element stored a value of default arg.
+        ~> sample_list is a list of lambda functions.
+        ~>lambda_function is not a literal, so it requires a parenthesis and because it a has a default arg equivalent to: sample_list[0]()
+        ~> since no args are given, each sample_list item/element stored a value of default arg.
 
-    sample_list[](0)'s default arg: 1    
-    sample_list[](1)'s default arg: 2
-    sample_list[](2)'s default arg: 3
-    sample_list[](3)'s default arg: 4   
+    sample_list[0]()'s default arg: 1    
+    sample_list[1]()'s default arg: 2
+    sample_list[2]()'s default arg: 3
+    sample_list[3]()'s default arg: 4   
     """
+    print(f"Lambda function at item {counter} of the list: {lamda_function()}")
     counter+=1
 
 """
 Lambda with Multiple Statements:
 
-    Lambda functions do not allow multiple statements, however, we can create two lambda functions and then call the other lambda function as a parameter to the first function.
+    ~>Lambda functions do not allow execution of multiple statements, however, we can create two lambda functions and then call the other lambda function as a parameter to the first function.
 """
 multi_val_operation=lambda x, y:(print("x+y=", x+y), print("x*y=", x*y))
 multi_val_operation(x=42, y=5)
@@ -75,12 +81,12 @@ multi_val_operation(x=42, y=5)
 """
 Using lambda with map():
 
-The map() function in Python takes in a function and a list as an argument. 
-The function is called with a lambda function and a new list is returned which contains all the lambda-modified items returned by that function for each item. 
+    ~>The map() function in Python takes in a function and a list as an argument. 
+    ~>The function is called with a lambda function and a new list is returned which contains all the lambda-modified items returned by that function for each item. 
 """
 
 a_list=[1, 2, 3, 4, 5]
-a_list=map(lambda x:x**2, a_list)
+a_list=map(lambda x:x**2, a_list)#A map function is an iterator that vectorizes each element in the iterable i.e. list here and aplies the given function on each element with the need of a loop and returns a map object
 a_list=list(a_list)
 print(a_list)
 
@@ -89,14 +95,15 @@ string=map(lambda char: char.upper() if char.isalpha() else char, string)
 string = ''.join(string)
 print(string)
 
-#Example of a higher order function:
+#Using lambda in a higher order function:
 numbers:list[int]=[3, 4, 5, 6, 7]
 
-def map_list(lambda_func, list):
-    result=[]
-    for item in list:
-        new_item=lambda_func(item)
+#Let's the make a DIY map function
+def map_list(function, iterable):
+    result=[] #could be any iterable(list, string, tuple, dict) etc...
+    for item in iterable:
+        new_item=function(item)
         result.append(new_item)
     return result
 
-print(map_list(lambda_func=lambda x: x**3, list=numbers))
+print(map_list(function=lambda x: x**3, iterable=numbers))
