4b4ab50a856efab42c917601c7fbd1ecd6de3b2a
Harsh Sharma <itisharshsharma18@gmail.com>
2025-10-05 10:24:34 +0530

    Changes made: 1. Removed the execption handling because the context managers ('with' & 'as') can close the file automatically... 2. Improved the comments.

--- SyntaxAndMethods/FileHandling/Writing_Files.py
+++ SyntaxAndMethods/FileHandling/Writing_Files.py
@@ -1,72 +1,60 @@
-#writing files in python .txt, .json, .csv
+#Writing files in python .txt, .json, .csv
 #Pending .xlsx and .zip
 
+#1. Text files:
 txt_data = input("Enter: ")
 txt_file_path = "/home/vhvhs/outputOfWrittingFile.txt"
 
-with open(txt_file_path, 'w') as file:# with is statement used to wrap the code i.e. closes the file as soon as it's done reading
     # open(), the first parameter will return as the file object 
     # the second parameter, 'w' is the writing mode for existing files, it also creates a new file if file does not exist.
+with open(txt_file_path, 'w') as file:# with is statement used to wrap the code i.e. closes the file as soon as it's done reading
         file.write(txt_data)
-try:
-    with open(txt_file_path, 'x') as file:
+
+with open(txt_file_path, 'x') as file:
     # open(), the first parameter will return as the file object 
     # the 'x' is EXCLUSIVE to only creating new files and writting them, it WILL raise an error if this file already exists.
         file.write(txt_data)
         print(f"text file {txt_file_path} is created now")
 
-except FileExistsError:#needed for 'x' OpenTextMode
-    print("This file exists already.")
-except PermissionError:
-    print("Error: File read permission denied.")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
-else:
-    print("For appending/updating a file's data. ")
-    txt_data2=input("Something to append: ")
+print("For appending/updating a file's data. ")
+txt_data2=input("Something to append: ")
 
-    with open(txt_file_path, 'a') as file:# appened mode
-        file.write("\n"+txt_data2+"\n")
-        print(f"text file {txt_file_path} is appended/updated now")
+with open(txt_file_path, 'a') as file:# appened mode
+    file.write("\n"+txt_data2+"\n")
+    print(f"text file {txt_file_path} is appended/updated now")
+#for lists 
+print("For appending/updating a file's data.")
+employee_list = ["Eugene", "Mayweather", "Sourbee"]
 
-    #for lists 
-    print("For appending/updating a file's data.")
-    employee_list = ["Eugene", "Mayweather", "Sourbee"]
+with open(txt_file_path, 'a') as file:
+    for employee in employee_list:
+        file.write(employee+"\n")
+    print(f"text file {txt_file_path} is appended/updated now for the list")
 
-    with open(txt_file_path, 'a') as file:
-        for employee in employee_list:
-            file.write(employee+"\n")
-        print(f"text file {txt_file_path} is appended/updated now for the list")
-
-
+#2. JSON
 import json
 json_file_path="/home/vhvhs/OutputOfJsonFile.json"
 json_data={}
 iteration=int(input("How many data entries = "))
+
 for i in range(iteration):#setting up a user-input dictionary named json_data.
     key = input(f"key {i} = ")
     value= input(f"value of {key} = ")
     json_data[key]=value
 print("-"*20)
-try:
-    with open(json_file_path, 'w') as json_file:
-        for k, v in json_data.items():
-            json_file.write(f"{k}={v}\n")
-        
-except FileNotFoundError:
-    print(f"Error: File not found at '{json_file_path}'")
-except PermissionError:
-    print("Error: File read permission denied.")
-except Exception as e:
-    print(f"An unexpected error occurred.")
-import csv
 
+with open(json_file_path, 'w') as json_file:
+    for k, v in json_data.items():
+        json_file.write(f"{k}={v}\n")
+
+
+#3. CSV = comma separated values, they are used for data like Microsoft excel or Google Sheets
+import csv
 print("For .csv files")
-#csv = comma separated values, they are used for data like Microsoft excel or Google Sheets
 
 employee_data = [["Name", "Age", "Profession"],
-                          ["Harsh", "18", "Unemployed"],
-                          ["Mango", "0", "Fruit"]] 
+                 ["Harsh", "18", "Unemployed"],
+                 ["Mango", "0", "Fruit"]] 
 
 csv_file_path = "/home/vhvhs/OutputOfCSVFile.csv"
 
