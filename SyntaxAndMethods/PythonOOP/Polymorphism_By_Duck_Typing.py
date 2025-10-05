50179d91c273b4a65b414df531d29309259bcccd
Harsh Sharma <itisharshsharma18@gmail.com>
2025-10-05 10:37:11 +0530

    Changes: 1. Capitalized the class names 2. Type hint in animals list

--- SyntaxAndMethods/PythonOOP/Polymorphism_By_Duck_Typing.py
+++ SyntaxAndMethods/PythonOOP/Polymorphism_By_Duck_Typing.py
@@ -1,42 +1,45 @@
 #Duck Typing is another method to achevive polymorphism besides Inheritance
 #It follows the principle "If it looks like a duck, If it quacks like a duck, it must be a duck"
 
-#In order to be duck typed an object must required attributes
+#In order to be duck typed, an object must require attributes
 
 class Animal:
+    alive = True
     def __init__(self, speech):
-        self.alive = True
         self.speech = speech
+
     def speak(self):
         print(self.speech)
-class dog(Animal):
+class Dog(Animal):
     def __init__(self, speech="Bark!"):
         super().__init__(speech)
 
     def __str__(self):
         return "Dog"
     
-class cat(Animal):
+class Cat(Animal):
     def __init__(self, speech="Meow!"):
         super().__init__(speech)
 
     def __str__(self):
         return "Cat"
     
-#now making a non-inherited class, car
-class car:
+#now making a non Animal inherited class, Car
+class Car:
     alive = False
-    def __init__(self, speech="Honk!"):
+    def __init__(self , speech="Honk!"):
         self.speech = speech
 
     def speak(self):
         print(self.speech)
 
     def __str__(self):
-        return "MF Car"
-animals = [car(), cat(), dog() ]#animals list is different from the Animal class
+        return "Car"
+    #animals list is different from the Animal class
+animals: list[object] = [Car(), Cat(), Dog() ]
 
 for animal in animals:
-    print(animal," = ",animal.alive)
+    print(animal.__str__())
+    print(animal," = ", animal.alive)
     animal.speak()
     print()
