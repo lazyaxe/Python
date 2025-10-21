4d2929a53a7642ea040178cadec7152c946184e4
Harsh Sharma <itisharshsharma18@gmail.com>
2025-10-21 20:54:42 +0530

    Changes: 1.  Massive upgrade to code comments 2. Changed the variable names to meaningful ones... 3. Did a few experiments with exeception handling.

--- SyntaxAndMethods/FileHandling/File_Detection.py
+++ SyntaxAndMethods/FileHandling/File_Detection.py
@@ -1,43 +1,59 @@
-#make a file(.txt) in the same folder(recommended)
-#use a relative file path or an absolute file path
-print()
+"""
+FILE DETECTION IN Python:
+~> File detection is used to detect the existence of files/directory.
+~> A path to the file is needed for the check.
+~> A method os.path.exists() is needed to check.
+~> Both libraries and directories can be checked.
+~> Both a relative file path (filename) or an absolute file path (/path/to/location) can be used.
+"""
 import os
 
-file_path = "SyntaxAndMethods/SampleFile.txt"# using is a relative file path, because we are inside a folder which is inside an another folder or at another directory
+# Using is a relative file path, because we are inside a folder which is inside an another folder or at another directory
+file_path1 = "SyntaxAndMethods/SampleFile.txt"
 
-if os.path.exists(file_path):
-    print(f"The location '{file_path}' exits.")
+if os.path.exists(file_path1):
+    print(f"The location '{file_path1}' exits.")
 
 else:
     print("File could not be located.")
 
-#for absolute path
-filepath="/home/vhvhs/python/SyntaxAndMethods/SampleFile.txt" #use / or \\ or //instead of \ because python has commands like \t for tab etc...which can cause execptions during file detection.
+#2. Absolute Path
+
+# NOTE:
+# ~> Use / or \\ or //instead of \ because python has commands like \t for tab etc...which can cause execptions during file detection.
+
+file_path2="/home/vhvhs/python/SyntaxAndMethods/SampleFile.txt" 
+
 try:
-   if not os.path.exists(filepath):
-       raise FileNotFoundError("The file path does not exist!")
-except FileNotFoundError as fileNotFound:
-    print(fileNotFound)
+   if not os.path.exists(file_path2):
+       raise FileNotFoundError
+   
+except FileNotFoundError as FNF:
+    FNF = f"{file_path2} does not exist!"
+    print(FNF)
+
 else:
-    print(f"File path={filepath} exists!")
+    print(f"File path={file_path2} exists!")
 
-#To check that we are accessing a file or a directory we use isfile() and isdir() functions.
-file_path2 = "/home/vhvhs/python"
+#3. Check if it's a file or a directory(dir) by isfile() and isdir():
+dir_path = "/home/vhvhs/python"
+
 try:
-    if os.path.exists(file_path2):
-        print(f"Location {file_path2} exists!")#there's no way to check id this a file or a directory.
+    if os.path.exists(dir_path):
+        print(f"Location {dir_path} exists!")
 
-        if os.path.isfile(file_path2):#this return a boolean
+    #Now to check if this a file of directory
+        if os.path.isfile(dir_path):
             print("This is a file.")
 
-        elif os.path.isdir(file_path2):
+        elif os.path.isdir(dir_path):
             print("This is a directory.")
         
         else:
             raise FileNotFoundError("There's no File/Directory here.")
 
-except FileNotFoundError as fileNotFound:
-    print(fileNotFound)
+except FileNotFoundError as FNF:
+    print(FNF)
 
 except PermissionError:
     print("Permission Denied!")
