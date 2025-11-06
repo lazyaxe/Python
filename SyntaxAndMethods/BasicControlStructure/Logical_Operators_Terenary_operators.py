de04d88ac205e82a99c450174e9eb0da9c99c77b
Harsh Sharma <itisharshsharma18@gmail.com>
2025-11-06 22:25:14 +0530

    Changes: Improved code documentation

--- SyntaxAndMethods/BasicControlStructure/Logical_Operators_Terenary_operators.py
+++ SyntaxAndMethods/BasicControlStructure/Logical_Operators_Terenary_operators.py
@@ -1,24 +1,24 @@
+# LOGICAL OPERATORS
 """
-Logical operators such as 'or', 'and' and 'not' are based on boolean algebra.
+~> Logical operators such as 'or', 'and' and 'not' are based on boolean algebra.
+~> They can be used in if-elif statements to make conditions more flexible by user-specific demands.
 
-They can be used in if-elif statements to make conditions more flexible by user-specific demands.
+1. and
+    if a_condition:
+        if another_condition:
+            #code
 
-and:
-if a_condition:
-    if another_condition:
+2. or
+    if a_condition:
+        #code
+    elif another_condition:
         #code
 
-or:
-if a_condition:
-    #code
-elif another_condition:
-    #code
-
-not:
-if !a_condition:
-    #code
-
-
+3. not
+    if a_condition:
+        continue # pass
+    else:
+        #code
 """
 temp = 20 
 is_weather_good = True
@@ -31,39 +31,38 @@ elif (temp <= 30 and is_weather_good and is_friends_available ):
     print("Your Friends are  available!")
     print("Everything checks out !")
     print("You can go to party")
-elif (temp>= 30 and not is_friends_available):
+elif (temp >= 30 and not is_friends_available):
     print("Its too HOT outside and Your Friends are not available!")
     print("You cannot go to Party")
-#inequality comparisions can also be done by
-elif (30>temp>10 and is_friends_available and not is_weather_good):
+elif (30 > temp > 10 and is_friends_available and not is_weather_good):
     print("Temp is good and your friends are available but weather is not good !")
     print("You cannot go to party !")
 
-#Conditional Expressions in Python, similar to TERNARY OPERATOR
+#Conditional Expressions in Python, similar to TERNARY OPERATOR in other programming languages...
     #for int/float
-a=int(input("Enter a Number:"))
+a = int(input("Enter a Number:"))
 
-even_or_not = (f"{a} is Even") if a%2==0 and a>0 else (f"{a} is not even")
+even_or_not = (f"{a} is Even") if a % 2 == 0 and a > 0 else (f"{a} is not even")
 #even_or_not will store this if or else as a string 
 print("Type of even_or_not is ",type(even_or_not))
 """
-Equivalent to:
+Equivalent to,
 
-if a%2==0 and a>0:
-    even_or_not="is even"
-else:
-    even_or_not="is not even"
+    if a % 2 == 0 and a > 0:
+        even_or_not = "is even"
+    else:
+        even_or_not = "is not even"
 """
 print(even_or_not)
 
 user = "admin"
-access_level_identifier = "FULL ACCESS" if user=="admin" else "LIMITED ACCESS"
+access_level_identifier = "FULL ACCESS" if user == "admin" else "LIMITED ACCESS"
 """
-Equivalent to:
+Equivalent to,
 
-if access_level_identifier=="admin":
-    access_level_identifier="FULL ACCESS"
-else:
-    ="LIMITED ACCESS"
+    if user == "admin":
+        access_level_identifier = "FULL ACCESS"
+    else:
+        access_level_identifier = "LIMITED ACCESS"
 """
 print(access_level_identifier)
