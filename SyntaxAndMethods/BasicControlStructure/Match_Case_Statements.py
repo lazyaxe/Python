ec1db60f91b37094966623e4d797052001d2ad10
Harsh Sharma <itisharshsharma18@gmail.com>
2025-11-06 22:50:41 +0530

    Changes: 1. Improved code readability 2. Removed the while loop since its not introduced before...

--- SyntaxAndMethods/BasicControlStructure/Match_Case_Statements.py
+++ SyntaxAndMethods/BasicControlStructure/Match_Case_Statements.py
@@ -1,13 +1,11 @@
+# MATCH-CASES:
 """
-Match-Cases:
-
-    Match-cases are very similar in terms of functionality in comparasion to if-elif-else statements as it also manipules the control flow.
-
-    Match Case conditions are equivalent to switch case in C/C++.
+    ~> Match-cases are very similar in terms of functionality in comparasion to if-elif-else statements as it also manipules the control flow.
+    ~> Match Cases are equivalent to switch cases in C/C++.
+    ~> They are preffered when there is a need of discrete comparision or where inequality operators are not needed
 """
-
-def day_of_the_week(day):
-    match day:
+def day_of_the_week(input_day):
+    match input_day:
         case 1:
             return "Sunday"
         case 2:
@@ -20,42 +18,39 @@ def day_of_the_week(day):
             return "Thursday"
         case 6:
             return "Friday"
-        case 0: #because 7%7=0
+        case 0: # because 7 % 7 = 0
             return "Saturday"
-        case _: #default case, similar to else
+        case _: # default case, similar to else
             return "Invaild input"
 """
-equivalent if else ladder:
+Equivalent if else ladder:
 
-if day=="Sunday":    
-    return 1
-elif day=="Monday":
-    return 2
-elif day=="Tuesday":
-    return 3
-elif day=="Wednesday":
-    return 4
-elif day=="Thrusday":
-    return 5
-elif day=="Friday":
-    return 6
-elif day=="Saturday":
-    return 7
-else: 
-    return "Invaild input"
+if input_day == 1:
+    return "Sunday"
+elif input_day == 2:
+    return "Monday"
+elif input_day == 3:
+    return "Tuesday"
+elif input_day == 4:
+    return "Wednesday"
+elif input_day == 5:
+    return "Thrusday"
+elif input_day == 6:
+    return "Friday"
+elif input_day == 7:
+    return "Saturday"
+else:
+    return "Invaild Input"
 """
-is_loop = True
-while is_loop:
-    day = input("Enter a day: ")
-    if (day.lower()=="e"):
-        print("Exiting.....")
-        is_loop=False
-    elif not day.isdigit():
+
+
+input_day = input("Enter a day: ")
+if not input_day.isdigit():
+    print("Invaild Input")
+else:
+    input_day = int(input_day)
+    if input_day <= 0:
         print("Invaild Input")
     else:
-        day=int(day)
-        if day<=0:
-            print("Invaild Input")
-        else:
-            day%=7
-            print(day_of_the_week(day))
+        input_day %= 7
+        print(day_of_the_week(input_day))
