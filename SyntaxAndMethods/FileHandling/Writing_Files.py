31c16e69e52bff693143667a72079a1344a68323
Harsh Sharma <itisharshsharma18@gmail.com>
2025-11-16 12:43:37 +0530

    Changes: 1. Added file modes and their explainations 2. Improved code doc

--- SyntaxAndMethods/FileHandling/Writing_Files.py
+++ SyntaxAndMethods/FileHandling/Writing_Files.py
@@ -1,18 +1,37 @@
-#Writing files in python .txt, .json, .csv
-#Pending .xlsx and .zip
+# WRITING FILES IN Python:.txt, .json, .csv
+# Next: .xlsx and .zip
+
+"""
+~> Files in Python can be usually accessed by:
+1. Using manual: file_object = open(file, mode) method
+2. Using Pythonic Context Managers i.e. automate file opening and closing.
+
+There are 5 write modes in Python
+1. 'w' : write-only
+2. 'w+' : write and read, creates a new file if not found
+3. 'wb' : write binary only
+4. 'wb+' : write and read binary, creates a new file if not found
+5. 'x' : write exclusivly new files only, DOES NOT creates a new file if not found
+6. 'a' : append only, creates a new file if not found
+7. 'a+' : append and write, creates a new file if not found
+8. 'ab' : append binary only, creates a new file if not found
+9. 'ab+' : append and read binary, creates a new file if not found
+
+~> Every write mode creates a new file if it does not exist, except for 'x'.
+
+"""
 
 #1. Text files:
 txt_data = input("Enter: ")
 txt_file_path = "/home/vhvhs/outputOfWrittingFile.txt"
 
-    # open(), the first parameter will return as the file object 
-    # the second parameter, 'w' is the writing mode for existing files, it also creates a new file if file does not exist.
-with open(txt_file_path, 'w') as file:# with is statement used to wrap the code i.e. closes the file as soon as it's done reading
+# open(), the first parameter will return as the file object 
+# The second parameter, 'w' is the writing mode for existing files, it also creates a new file if file does not exist.
+with open(file = txt_file_path, mode = 'w') as file:
         file.write(txt_data)
 
-with open(txt_file_path, 'x') as file:
-    # open(), the first parameter will return as the file object 
-    # the 'x' is EXCLUSIVE to only creating new files and writting them, it WILL raise an error if this file already exists.
+# The 'x' is EXCLUSIVE to only creating new files and writting them, it WILL raise an error if this file already exists.
+with open(file = txt_file_path, mode = 'x') as file:
         file.write(txt_data)
         print(f"text file {txt_file_path} is created now")
 
@@ -22,30 +41,32 @@ txt_data2=input("Something to append: ")
 with open(txt_file_path, 'a') as file:# appened mode
     file.write("\n"+txt_data2+"\n")
     print(f"text file {txt_file_path} is appended/updated now")
-#for lists 
+
+#For writting lists 
 print("For appending/updating a file's data.")
 employee_list = ["Eugene", "Mayweather", "Sourbee"]
 
 with open(txt_file_path, 'a') as file:
     for employee in employee_list:
-        file.write(employee+"\n")
+        file.write(employee + "\n")
     print(f"text file {txt_file_path} is appended/updated now for the list")
 
 #2. JSON
 import json
-json_file_path="/home/vhvhs/OutputOfJsonFile.json"
-json_data={}
-iteration=int(input("How many data entries = "))
 
-for i in range(iteration):#setting up a user-input dictionary named json_data.
+json_file_path = "/home/vhvhs/OutputOfJsonFile.json"
+json_data_as_dict = {}
+iteration = int(input("How many data entries = "))
+
+for i in range(iteration):
     key = input(f"key {i} = ")
-    value= input(f"value of {key} = ")
-    json_data[key]=value
-print("-"*20)
+    value = input(f"value of {key} = ")
+    json_data_as_dict[key] = value
+print("-" * 20)
 
 with open(json_file_path, 'w') as json_file:
-    for k, v in json_data.items():
-        json_file.write(f"{k}={v}\n")
+    for k, v in json_data_as_dict.items():
+        json_file.write(f"{k} = {v}\n")
 
 
 #3. CSV = comma separated values, they are used for data like Microsoft excel or Google Sheets
@@ -59,8 +80,8 @@ employee_data = [["Name", "Age", "Profession"],
 csv_file_path = "/home/vhvhs/OutputOfCSVFile.csv"
 
 with open(csv_file_path, 'w') as file:
-        
         writer = csv.writer(file)
+
         for employee_data_row in employee_data:
             writer.writerow(employee_data_row)
         print(f"CSV file {csv_file_path} is created now")
