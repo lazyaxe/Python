66cd9a0788d1df8a00c538b810505ade5442b536
Harsh Sharma <itisharshsharma18@gmail.com>
2025-11-16 11:07:02 +0530

    Changes: 1. Renamed the function 'needed_code' to 'main' for more conciseness 2. Showed a different/experimental way to use main function 3. Improved code documentation.

--- SyntaxAndMethods/IntermediatePython/IfNameMain.py
+++ SyntaxAndMethods/IntermediatePython/IfNameMain.py
@@ -1,12 +1,43 @@
-def needed_code():#you can also name it main():
-    print("This is the only part we need to run inside another file")
-    print("I only need this code, when i use this as a module ")
-def add(x,y):
-    return x+y#I also need this part of code
-print(__name__)#when i am running this program alone program its name is '__main__' 
-               #i.e. it's the main program, if i import then run it it will be the file's name(i.e. IfNameMain.py)
-if __name__=='__main__':
-    print("BLABLABLA!, THIS PART OF CODE IS NOT NEEDED FOR MODULES")
-    print("So this if condtion is only true when its the main program, not when its run by a module")
-    needed_code()
+#IF NAME == "MAIN" CONDITION
+"""
+~> The if __name__ == "__main__" is needed when we need to use a program as a module/library for another program.
+~> This condition helps us to encapsulate 
+"""
+def add(x, y):
+    return x + y
+
+def program_name():
+    #If running this program alone, the program its name is '__main__' i.e. it's the main program, if i import then run it it will be the file's name(i.e. IfNameMain.py)
+
+    print("The program's name : ", __name__)
+
+#Defining a main function in Python:
+#Create a Function Called main() to Contain the Code You Want to Run
+def main():
+    program_name()
+    print("I only need this code, when i use this as as the main program")
     print(add(1, 2))
+"""
+    This is executed if condition is only true when its the main program, not when it used as a module.
+"""
+if __name__=='__main__':
+    main()
+
+    #OR don't use the main function.
+    program_name()
+    print("I only need this code, when i use this as as the main program")
+    print(add(1, 2))
+
+#OR modify the main function such that it only executes in if __name__ == '__main__'
+
+"""
+def main():
+    if __name__ == "__main__":
+        program_name()
+        print("I only need this code, when i use this as as the main program")
+        print(add(1, 2))
+    else:
+        pass
+
+main()
+"""
