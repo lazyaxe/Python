6b408a4b5d7cf02d41adbe7e2841567cfc708158
Harsh Sharma <itisharshsharma18@gmail.com>
2025-11-16 11:14:20 +0530

    Changes: 1. Added the Heirarchial Inheritance's explaination. 2. Better code documentation. 3. Cleaned up the code.

--- SyntaxAndMethods/PythonOOP/Inheritance.py
+++ SyntaxAndMethods/PythonOOP/Inheritance.py
@@ -1,42 +1,43 @@
+#Inheritance in Python:
 """
-    Inheritance:
+    What is Inheritance?
     ~> Inheritance is OOP paradigm in which the functionality of a class i.e. it's methods, attributes and constructor(requires super function) can be inherited/transferred to other class(inherrited class)...
     
     ~> The class which does the inheritance is the Parent/(Super/Base) class and the class with inherits is the Child/Sub class.
     
     ~> Inheritance promotes code resuablity as the methods and attributes can be used by other class.
     
-    ~> Only the inherrited class can access the protected and private atrributes and methods.
+    ~> Only the inherrited class can access the protected atrributes and methods.
 
     Types of inherritance:
     1. Simple Inherritance
-    2. Multi-level Inherritance
-    3. Multiple Inherritance
-    4. Hybrid inherritance
-    5.Duck Typing(not covered in this file)
+    2. Heirarchial Inheritance
+    3. Multi-level Inherritance
+    4. Multiple Inherritance
+    5. Hybrid inherritance
 """
 
-#these subclasses inherit from animal class they also inherit the attributes/members getters/setters and constructors too
+#Sub-classes that inherit from Animal class also inherit its attributes/members, methods and constructor too
 class Animal:
-    def  __init__(self, name:str , age: int=0)->None:
-        self.isAlive=True
-        self.name=name
-        self._age= int(age)
+    def  __init__(self, name: str , age: int = 0) -> None:
+        self.isAlive = True
+        self.name = name
+        self._age = age
 
-    def eat(self)->None:
+    def eat(self) -> None:
         print(f"{self.name} is eating now.")
 
-    def sleep(self)->None:
+    def sleep(self) -> None:
         print(f"{self.name} is sleeping now.")
 
-    def get_age(self)->None:
+    def get_age(self) -> int:
         return self._age
     
-    def set_age(self, new_age)->None:
-        if self._age>=0:
-            self._age=new_age
+    def set_age(self, new_age) -> None:
+        if self._age >= 0:
+            self._age = new_age
             
-    def  display_age(self)->None:
+    def  display_age(self) -> None:
         print(f"{self.name} is {self._age} yrs old")
 
 #1. Simple inheritance:
@@ -54,36 +55,13 @@ print(dog1.get_age())
 dog1.display_age()
 print()
 
-#2.Heirarchial Inheritance, because now there are more than one child-class...
-class Horse(Animal): # class SubClassName(BaseClass)
+#2.Heirarchial Inheritance
+#When more than one class inherrit from the same parent class.
+
+#Horse Class also inherits Animal Class 
+class Horse(Animal):
     def make_sound(self)->None:
         print("Neighs...")
-
-
-class Donkey(Animal): # class SubClassName(BaseClass)
-    def make_sound(self)->None:
-        print("Brays...")
-
-#3. Multiple inherritance 
-class Mule(Horse, Donkey):
-    pass
-    """
-        ~>Because this class inherits from two classes which have same methods(but different execution i.e. different sound) there would be clash.
-        ~>Python resolves this by: Which class is the first argument will overwrite the other class method i.e. Mule will neigh.
-    """
-
-#4. Hybrid Inheritance
-"""
-~> The animal class performing hybrid inheritance which does heirarchial inheritance(Dog, Horse Mule). 
-~> Also does Multi-level inheritance with Animal->Horse->Mule.
-~> Abomination inherits from an independent class Bug and inherited class Horse 
-"""
-class Bug:
-    def make_sound(self)->None:
-        print("Thraxxan noises...")
-
-class Abomination(Bug, Horse):
-    pass
 
 horse1 = Horse("Tom", 23)
 print(horse1.isAlive)
@@ -92,16 +70,44 @@ horse1.eat()
 horse1.sleep()
 print()
 
-donkey1= Donkey("Jerry", 23)
+#Donkey Class also inherits Animal Class 
+#So Animal class is the base class of 3 classes.
+class Donkey(Animal):
+    def make_sound(self)->None:
+        print("Brays...")
+
+donkey1 = Donkey("Jerry", 23)
 print(donkey1.isAlive)
 print(donkey1.name)
 donkey1.eat()
 donkey1.sleep()
 
-mule1 =Mule(name="Nibs", age=5)
+#3. Multiple inherritance 
+"""
+    ~> When a class inherits from two different classes which have same methods(but different execution i.e. different make_sound method) there would be a clash of which class's method should be used.
+    ~> This known as the "Diamond Problem".
+    ~> Python resolves Diamond Problem by: "The class which is the first argument will override the other class method of the same method name" i.e. Mule will "Neigh" not "Bray"...
+"""
+class Mule(Horse, Donkey):
+    pass
+
+mule1 = Mule(name = "Terry", age = 5)
 print(mule1.name)
 mule1.make_sound()
 
-a=Abomination(name="A mistake", age=18)
+#4. Hybrid Inheritance
+"""
+~> The animal class performing hybrid inheritance which does heirarchial inheritance(Dog, Horse Mule). 
+~> Also does Multi-level inheritance with Animal->Horse->Mule.
+~> Abomination inherits from an independent class Insect and inherited class Horse 
+"""
+class Insect:
+    def make_sound(self) -> None:
+        print("Thraxxan noises...")
+
+class Abomination(Insect, Horse):
+    pass
+
+a = Abomination(name = "A mistake", age = 18)
 print(a.name)
 a.make_sound()
