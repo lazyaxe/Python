202512c33c2eb99fc755bf2235271b7e13d88409
Harsh Sharma <itisharshsharma18@gmail.com>
2026-02-16 19:59:40 +0530

    Changes: More readability

--- SyntaxAndMethods/BasicControlStructure/types_of_variables.py
+++ SyntaxAndMethods/BasicControlStructure/types_of_variables.py
@@ -1,21 +1,27 @@
+# Datatypes and variables in Python
 """
-A variable is a container storing a value =(string, integer, float, boolean) data type is automatically identified 
-Python can have indentation errors.
+~> A variable is a container storing a value that could be a string, integer, float, boolean. Datatype is automatically identified (at run-time)
+~> Variables are directly assigned in Python i.e. No declaration only initalization
+~> Python can have indentation errors.
+~> Semi colons can be used!(but not recommended)
 """
 #Stings in Python
-firstName = "Harsh" #string data type
-print(f"Hello {firstName}!")#f is the f-line|f-string and {} is the place holder
+firstName = "Harsh"
+
+#f is the f-line|f-string and {variable_name} is the place holder for the variabel
+print(f"Hello {firstName}!")
+
+#print function can have single quotes '' or double quotes ""
 print(f'single quotes also work {firstName}!')
 
 # Integers/Numeric data-types in Python:
 
 #int datatype
-age = 18;#semi colons can be used!(but not recommended)
-
+age = 18;
 print(f"{firstName} is {age} years old!")
 
 #float datatype: 
-float_value=69.420
+float_value = 69.420
 print(f"Float = {float_value}")
 
 #boolean, can either be True(1) or False(0):
@@ -27,27 +33,32 @@ print(f"Are you a Teacher? {is_response}.")
 #Typecasting is the Process of converting one datatype into another data type
 # str() int() float() bool()
 
+#gpa is a float data type
 gpa = 5.3 
-"""
-gpa is a float data type
-"""
 print(type(age))#checking datatype.
 
 # float=>int typecast
 gpa = int(gpa) 
 print("now it is ", type(gpa))#checking the type conversion, only type() won't give anything
 
+#int => string typecast
 age = str(age)
-print(type(age))#check
 
- #concatenating(joining) strings
-age += "1"#this joins string "1" to string age "28"
+#check/print the datatype
+print(type(age))
+
+#concatenating(joining) strings
+#this joins string "1" to string age "28"
+age += "1"
 print(age)
 
-# converting a string to a bool datatype
 string = "1"
-print(f"This", string, "also works...")
+print("This", string, "also works...")
 
-string = bool(string)#Non-empty string =True, Empty string=False
+# converting a string to a bool datatype
+
+#Non-empty string = true, Empty string = false
+string = bool(string)
+
 #bool is false when string is empty, it can be used as a empty string detector
-print(f"is string non-empty ? {string}")
+print(f"Is the string not empty ? : {string}")
